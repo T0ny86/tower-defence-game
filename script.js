a93a9703946d746157c96d3c4e1f07dcab7cdc8b
Tanous Atallah <tanous.atallah@gmail.com>
2023-02-07 15:37:28 +0100

    project setup, done

---
+++ script.js
@@ -0,0 +1,11 @@
+/** @type{HTMLCanvasElement} */
+
+window.addEventListener('load',function () {
+    /** @type {HTMLCanvasElement} */
+    // canvas setup
+    const canvas = document.getElementById('canva')
+    const ctx = canvas.getContext("2d")
+    canvas.width = 1280
+    canvas.height = 720
+    
+})
