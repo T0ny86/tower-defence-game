18d18a48b1752ba8b2bcc2c75b6d7e9c0f733149
Tanous Atallah <tanous.atallah@gmail.com>
2023-02-07 19:41:04 +0100

    add basic classes

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
 /** @type{HTMLCanvasElement} */
 
-window.addEventListener('load',function () {
-    /** @type {HTMLCanvasElement} */
-    // canvas setup
-    const canvas = document.getElementById('canva')
-    const ctx = canvas.getContext("2d")
-    canvas.width = 1280
-    canvas.height = 720
-    
-})
+window.addEventListener("load", function () {
+  /** @type {HTMLCanvasElement} */
+  // canvas setup
+  const canvas = document.getElementById("canva");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 1280;
+  canvas.height = 720;
+
+  ctx.fillStyle = "white";
+
+  class Player {
+    constructor(game) {
+      this.game = game;
+      this.collisionX = this.game.width * 0.5;
+      this.collisionY = this.game.height * 0.5;
+      this.collisionRadius = 40;
+    }
+    draw(context) {
+      context.beginPath();
+      context.arc(
+        this.collisionX,
+        this.collisionY,
+        this.collisionRadius,
+        0,
+        Math.PI * 2
+      );
+      context.save();
+      context.globalAlpha = 0.5;
+      context.fill();
+      context.restore();
+    }
+  }
+
+  class Game {
+    constructor(canvas) {
+      this.canvas = canvas;
+      this.width = this.canvas.width;
+      this.height = this.canvas.height;
+      this.player = new Player(this);
+    }
+    render(context) {
+      this.player.draw(context);
+    }
+  }
+
+  const game = new Game(canvas);
+  game.render(ctx);
+  function animation() {}
+});
