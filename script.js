9525c3fc9f30c079ac8cba7a02eea0ad3c0b5b1e
Tanous Atallah <tanous.atallah@gmail.com>
2023-02-18 19:41:18 +0100

    player move is done

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ window.addEventListener("load", function () {
   canvas.height = 720;
 
   ctx.fillStyle = "white";
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = "white";
 
   class Player {
     constructor(game) {
@@ -16,6 +18,11 @@ window.addEventListener("load", function () {
       this.collisionX = this.game.width * 0.5;
       this.collisionY = this.game.height * 0.5;
       this.collisionRadius = 40;
+      this.speedX = 0;
+      this.speedY = 0;
+      this.dx = 0;
+      this.dy = 0;
+      this.speedModifier = 20;
     }
     draw(context) {
       context.beginPath();
@@ -30,6 +37,35 @@ window.addEventListener("load", function () {
       context.globalAlpha = 0.5;
       context.fill();
       context.restore();
+      context.stroke();
+      // draw the direction line
+      context.beginPath();
+      context.moveTo(this.collisionX, this.collisionY);
+      context.lineTo(this.game.mouse.x, this.game.mouse.y);
+      context.stroke();
+    }
+    update() {
+      this.dx = this.game.mouse.x - this.collisionX;
+      this.dy = this.game.mouse.y - this.collisionY;
+      // if we want continuos constant speed, we need to calculate the 'Hypotenuse':
+      const distance = Math.hypot(this.dy, this.dx);
+      if (distance > this.speedModifier) {
+        this.speedX = this.dx / distance || 0; // OR zero " || 0 " just a backup if any values is not defined
+        this.speedY = this.dy / distance || 0;
+      }else{
+        this.speedX = 0;
+        this.speedY = 0;
+      }
+      // console.log(`distance: ${distance.toFixed(2)} | speedX: ${this.speedX.toFixed(2)} | speedY: ${this.speedY.toFixed(2)}`);
+
+      /* this method will not give us a continuous constant 'same' speed,
+      at beginning the speed is maximum, and then will be slower
+      at begining :
+      this.speedX = this.dx / 10;
+      this.speedY = this.dy / 10;
+      */
+      this.collisionX += this.speedX * this.speedModifier;
+      this.collisionY += this.speedY * this.speedModifier;
     }
   }
 
@@ -59,17 +95,26 @@ window.addEventListener("load", function () {
         // console.log(this.mouse.x , this.mouse.y);
       });
       canvas.addEventListener("mousemove", (e) => {
-        this.mouse.x = e.offsetX;
-        this.mouse.y = e.offsetY;
+        if (this.mouse.pressed) {
+          this.mouse.x = e.offsetX;
+          this.mouse.y = e.offsetY;
+        }
         // console.log(this.mouse.x , this.mouse.y);
       });
     }
     render(context) {
       this.player.draw(context);
+      this.player.update();
     }
   }
 
   const game = new Game(canvas);
-  game.render(ctx);
-  function animation() {}
+
+  function animation() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.render(ctx);
+    requestAnimationFrame(animation);
+  }
+
+  animation();
 });
